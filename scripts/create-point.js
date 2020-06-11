fd608708fe69d0a7f25d990c4b275570e9c5b382
Raiff Smith Said <raiff.said@gmail.com>
2020-06-10 23:10:53 -0300

    feat: Alphabetical states ordening

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -1,13 +1,25 @@
- function populateUFs() {
-   const ufSelect = document.querySelector("select[name=uf]")
+function populateUFs() {
+  const ufSelect = document.querySelector("select[name=uf]")
 
-   fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
-    .then((response) => response.json())
-    .then(states => {
-      for (const state of states) {
-        ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+  fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+   .then((response) => response.json())
+   .then(states => {
+
+     // Ordenando por ordem alfabética
+     states.sort((a, b) => {
+      if (a.nome > b.nome) {
+        return 1;
+      } else if (a.nome < b.nome) {
+        return -1;
+      } else {
+        return 0;
       }
-    });
- }
+     });
+     
+     for (const state of states) {
+       ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+     }
+   });
+}
 
  populateUFs();
