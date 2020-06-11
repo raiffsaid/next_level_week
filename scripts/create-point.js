29058bb2454ec272faa1d43701b747857a8cb1d5
Raiff Smith Said <raiff.said@gmail.com>
2020-06-10 23:46:03 -0300

    feat: Cities listing options

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -15,11 +15,41 @@ function populateUFs() {
         return 0;
       }
      });
-     
+
      for (const state of states) {
        ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
      }
    });
 }
 
- populateUFs();
+populateUFs();
+
+function getCity(event) {
+  const citySelect = document.querySelector("select[name=city]")
+  const stateInput = document.querySelector("input[name=state]")
+
+  const ufValue = event.target.value;
+
+  const indexOfSelectedState = event.target.selectedIndex;
+  stateInput.value = event.target.options[indexOfSelectedState].text;
+
+
+  const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+
+  fetch(url)
+  .then((response) => response.json())
+  .then(cities => {
+    for (const city of cities) {
+      citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+    }
+
+    citySelect.disabled = false;
+  });
+}
+
+document
+  .querySelector("select[name=uf]")
+  .addEventListener("change", getCity);
+
+
+// https://servicodados.ibge.gov.br/api/v1/localidades/estados/{UF}/municipios
